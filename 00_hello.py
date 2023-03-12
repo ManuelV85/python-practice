6d2b5603d6695f1e0329bbcaa004c0030abd63f7
Manuel Villate <manuel.villater@gmail.com>
2023-03-11 21:20:25 -0600

    prueba de vs

--- 00_hello.py
+++ 00_hello.py
@@ -9,3 +9,5 @@ print(5 * 9) #multiplication (*)
 print(50 / 4) #division
 
 #Python es un lenguaje orientado a Objetos.
+
+print("prueba de vs, estooy haciendo la configuración para guardar en github")
